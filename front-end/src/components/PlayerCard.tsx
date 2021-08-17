c76b2abac509a2867d1e9ee29a3b04d0eb239159
Duncan Magill <duncanmagill@hotmail.co.uk>
2021-08-17 13:28:12 +0100

    created

--- front-end/src/components/PlayerCard.tsx
+++ front-end/src/components/PlayerCard.tsx
@@ -2,18 +2,17 @@ import "../styles/playerCard.css";
 
 function PlayerCard() {
   return (
-    <div className="card">
-      <div className="imageContainer">
-        <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIF.48wyEFFiX6i3Pf1YtpwOTw%26pid%3DApi&f=1"
-          alt="Lionel Messi"
-        />
+    <a href="#"> 
+      <div className="card">
+        <div className="imageContainer">
+          <img
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIF.48wyEFFiX6i3Pf1YtpwOTw%26pid%3DApi&f=1"
+            alt="Lionel Messi"
+          />
+        </div>
+        <h3>Lionel Messi</h3>
       </div>
-      <div>
-        <p>Lionel</p>
-        <p>Messi</p>
-      </div>
-    </div>
+    </a>
   );
 }
 
