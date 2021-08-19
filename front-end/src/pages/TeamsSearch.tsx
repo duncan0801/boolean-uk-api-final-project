215556b0d8682e8a5c69157ad0e41f3c60f6d062
Elio Mehmeti <mehmetielio95@gmail.com>
2021-08-19 18:58:56 +0100

    Style Things

--- front-end/src/pages/TeamsSearch.tsx
+++ front-end/src/pages/TeamsSearch.tsx
@@ -2,6 +2,38 @@ import React, { useEffect } from "react";
 import useStore from "../store";
 import { Grid } from "@material-ui/core";
 import CardSmall from "../components/CardSmall";
+import { makeStyles } from "@material-ui/core/styles";
+
+const cardContainerStyle = makeStyles({
+  container: {
+    backgroundColor: "#fabc3c",
+    padding: "4rem 0",
+    margin: "0",
+  },
+});
+
+function ContainerStyled() {
+  const teams = useStore((store) => store.teams);
+
+  const classes = cardContainerStyle();
+
+  return (
+    <Grid container justify="center" spacing={8} className="infoSection">
+      {teams.map((team) => {
+        return (
+          <Grid item>
+            <CardSmall
+              resource="teams"
+              id={team.id}
+              imageUrl={team.badgeUrl}
+              name={team.name}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
 
 function TeamsSearch() {
   const fetchTeams = useStore((store) => store.fetchTeams);
@@ -14,30 +46,7 @@ function TeamsSearch() {
   if (teams) {
     return (
       <main>
-        <div className="viewSection">
-          <Grid
-            container
-            direction="row"
-            alignItems="center"
-            justifyContent="center"
-            style={{ minWidth: "100vw" }}
-          >
-            <Grid container className="infoSection">
-              <Grid item>
-                {teams.map((team) => {
-                  return (
-                    <CardSmall
-                      resource="teams"
-                      id={team.id}
-                      imageUrl={team.badgeUrl}
-                      name={team.name}
-                    />
-                  );
-                })}
-              </Grid>
-            </Grid>
-          </Grid>
-        </div>
+        <ContainerStyled />
       </main>
     );
   }
