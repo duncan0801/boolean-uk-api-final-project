c3167b98719487e6392f7cbf92558090b7ec22db
Elio Mehmeti <mehmetielio95@gmail.com>
2021-08-19 23:37:51 +0100

    Styling the Search Pages

--- front-end/src/pages/LeaguesSearch.tsx
+++ front-end/src/pages/LeaguesSearch.tsx
@@ -8,6 +8,64 @@ import {
   Grid,
 } from "@material-ui/core";
 import CardSmall from "../components/CardSmall";
+import SearchBar from "material-ui-search-bar";
+import { makeStyles } from "@material-ui/core/styles";
+
+const cardContainerStyle = makeStyles({
+  container: {
+    backgroundColor: "#fabc3c",
+    padding: "4rem 0",
+    margin: "0",
+  },
+});
+
+const searchBarStyle = makeStyles({
+  searchbar: {
+    width: "100%",
+  },
+  container: {
+    display: "grid",
+    placeContent: "center",
+    backgroundColor: "#fabc3c",
+  },
+});
+
+function ContainerStyled() {
+  const leagues = useStore((store) => store.leagues);
+
+  const classes = cardContainerStyle();
+
+  return (
+    <Grid container spacing={10} justify="center" className={classes.container}>
+      {leagues.map((league) => {
+        return (
+          <Grid item>
+            <CardSmall
+              resource="leagues"
+              id={league.id}
+              imageUrl={league.imageUrl}
+              name={league.name}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
+
+function SearchBarEdit() {
+  const classes = searchBarStyle();
+
+  return (
+    <FormControl className={classes.container}>
+      <SearchBar
+        className={classes.searchbar}
+        onChange={() => console.log("onChange")}
+        onRequestSearch={() => console.log("onRequestSearch")}
+      />
+    </FormControl>
+  );
+}
 
 function LeaguesSearch() {
   const fetchLeagues = useStore((store) => store.fetchLeagues);
@@ -22,31 +80,8 @@ function LeaguesSearch() {
   if (leagues) {
     return (
       <main>
-        <div className="viewSection">
-          <Grid
-            container
-            direction="row"
-            alignItems="center"
-            justifyContent="center"
-            style={{ minWidth: "100vw" }}
-          >
-            <Grid container className="infoSection">
-              <Grid item>
-                {leagues.map((league) => {
-                  console.log(league.imageUrl);
-                  return (
-                    <CardSmall
-                      resource="leagues"
-                      id={league.id}
-                      imageUrl={league.imageUrl}
-                      name={league.name}
-                    />
-                  );
-                })}
-              </Grid>
-            </Grid>
-          </Grid>
-        </div>
+        <SearchBarEdit />
+        <ContainerStyled />
       </main>
     );
   }
--- front-end/src/pages/ManagersSearchPage.tsx
+++ front-end/src/pages/ManagersSearchPage.tsx
@@ -1,10 +1,73 @@
 import { useEffect } from "react";
 import useStore from "../store";
-import { Grid } from "@material-ui/core";
+import { Grid, FormControl } from "@material-ui/core";
 import CardSmall from "../components/CardSmall";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchBar from "material-ui-search-bar";
+
 function ManagersSearchPage() {
   const fetchManagers = useStore((state) => state.fetchManagers);
   const managers = useStore((state) => state.managers);
+
+  const cardContainerStyle = makeStyles({
+    container: {
+      backgroundColor: "#fabc3c",
+      padding: "4rem 0",
+      margin: "0",
+    },
+  });
+
+  const searchBarStyle = makeStyles({
+    searchbar: {
+      width: "100%",
+    },
+    container: {
+      display: "grid",
+      placeContent: "center",
+      backgroundColor: "#fabc3c",
+    },
+  });
+
+  function ContainerStyled() {
+    const classes = cardContainerStyle();
+
+    return (
+      <Grid
+        container
+        justify="center"
+        spacing={8}
+        className={classes.container}
+      >
+        {managers.map((manager) => {
+          let name = manager.firstName + " " + manager.lastName;
+          return (
+            <Grid item>
+              <CardSmall
+                resource="managers"
+                id={manager.id}
+                imageUrl={manager.imageUrl}
+                name={name}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    );
+  }
+
+  function SearchBarEdit() {
+    const classes = searchBarStyle();
+
+    return (
+      <FormControl className={classes.container}>
+        <SearchBar
+          className={classes.searchbar}
+          onChange={() => console.log("onChange")}
+          onRequestSearch={() => console.log("onRequestSearch")}
+        />
+      </FormControl>
+    );
+  }
 
   useEffect(() => {
     fetchManagers();
@@ -16,30 +79,8 @@ function ManagersSearchPage() {
   console.log(managers);
   return (
     <main>
-      <div className="viewSection">
-        {/* <Grid
-          container
-          direction="row"
-          alignItems="center"
-          justifyContent="center"
-          style={{ minWidth: "100vw" }}
-        > */}
-        <Grid container className="infoSection">
-          {managers.map((manager) => {
-            let name = manager.firstName + " " + manager.lastName;
-            return (
-              <Grid item>
-                <CardSmall
-                  resource="managers"
-                  id={manager.id}
-                  imageUrl={manager.imageUrl}
-                  name={name}
-                />
-              </Grid>
-            );
-          })}
-        </Grid>
-      </div>
+      <SearchBarEdit />
+      <ContainerStyled />
     </main>
   );
 }
